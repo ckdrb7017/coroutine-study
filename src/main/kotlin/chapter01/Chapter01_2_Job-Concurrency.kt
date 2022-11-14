de9f96f54a0e9b6df6b8b65dcaf4a2f603b6f186
changgyu <ckdrb7017@hanmail.net>
2022-11-14 21:45:12 +0900

    [Update] Job-Concurrency 관련 내용 추가

--- src/main/kotlin/chapter01/Chapter01_2_Job-Concurrency.kt
+++ src/main/kotlin/chapter01/Chapter01_2_Job-Concurrency.kt
@@ -3,7 +3,8 @@ package chapter01
 import kotlinx.coroutines.*
 
 fun Chapter01_Job_Concurrency() = runBlocking {
-    doOneTwoThree()
+//    doOneTwoThree()
+    doOneTwoThree2()
 }
 
 /*
@@ -13,18 +14,47 @@ fun Chapter01_Job_Concurrency() = runBlocking {
 * suspend 키워드는 함수에 suspension point를 만들 뿐, 코루틴 스코프가 아니다.
 *  = coroutineScope{} 같이 expression body로 표현하면 아래 launch를 실행할 수 있다.
 * */
-suspend fun doOneTwoThree() {
-    launch {
-        println("launch01: ${Thread.currentThread().name}")
+//suspend fun doOneTwoThree() {
+//    launch {
+//        println("launch01: ${Thread.currentThread().name}")
+//        delay(1000)
+//        println(3)
+//    }
+//    launch {
+//        println("launch01: ${Thread.currentThread().name}")
+//        println(1)
+//    }
+//    launch {
+//        println("launch01: ${Thread.currentThread().name}")
+//        delay(500)
+//        println(2)
+//    }
+//
+//    println(4)
+//}
+
+suspend fun doOneTwoThree2() = coroutineScope {
+    /*
+    * launch는 Job 객체를 반환한다.
+    * join은 suspension point를 가지고 있으며 해당 잡이 끝날때 까지
+    * join 이후의 코드는 join이 끝날때(launch가 완료 될 때) 까지 실행되지 않는다.
+    *
+    * */
+    val job = launch {
+        println("launch1: ${Thread.currentThread().name}")
         delay(1000)
         println(3)
     }
+
+    job.join()
+
     launch {
-        println("launch01: ${Thread.currentThread().name}")
+        println("launch2: ${Thread.currentThread().name}")
         println(1)
     }
+
     launch {
-        println("launch01: ${Thread.currentThread().name}")
+        println("launch3: ${Thread.currentThread().name}")
         delay(500)
         println(2)
     }
